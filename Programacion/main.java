01a1da8e67c9b7514365c524bd181c80fbef86a8
Rodrigo <rodrigobueno@iescastelar.com>
2023-09-25 17:59:29 +0200

    Tarea 1 programacion

--- Programacion/main.java
+++ Programacion/main.java
@@ -1,5 +1,100 @@
 public class main {
     public static  void main  (String[]args){
-        System.out.println("HOLA SOY RODRIGO Y ESTAS VIENDO DISNEY CHANNEL");
+        ejercicio1();
+        System.out.println("");
+        ejercicio2();
+        System.out.println("");
+
+        ejercicio3();
+        System.out.println("");
+
+        ejercicio4();
+        System.out.println("");
+
+        ejercicio5();
+        System.out.println("");
+
+        ejercicio6();
+        System.out.println("");
+
+        ejercicio7();
+    }
+
+    public static void ejercicio1 () {
+        System.out.println("Rodrigo");
+
+    }
+    public static void ejercicio2 (){
+        System.out.println("edad 20");
+        System.out.println("Correo Rodrigobueno@iescastelar.com");
+    }
+    public static void ejercicio3() {
+     System.out.print("Apple\tManzana");
+     System.out.println(" ");
+    System.out.print("Plane\tAvion");
+    System.out.println(" ");
+    System.out.print("Gun\tPistola");
+    System.out.println(" ");
+    System.out.print("Water\tAgua");
+    System.out.println(" ");
+    System.out.print("Sun\tSol");
+    System.out.println(" ");
+    System.out.print("Sky\tCielo");
+    System.out.println(" ");
+    System.out.print("Ground\tTierra");
+    System.out.println(" ");
+    System.out.print("Number\tNumero");
+    System.out.println(" ");
+    System.out.print("Letter\tCarta");
+    System.out.println(" ");
+    System.out.print("Car\tCoche");
+ }
+
+    public static void ejercicio4() {
+        System.out.println("+------------------------------------------------------+");
+        System.out.println("| Hora | Lunes | Martes | Miércoles | Jueves | Viernes |");
+        System.out.println("+------------------------------------------------------+");
+        System.out.println("|  3-4 |  Prog | Marcas |  Sistemas |  BBDD  |   FOL   |");
+        System.out.println("+------------------------------------------------------+");
+        System.out.println("|  4-5 |  Prog | Marcas |  Sistemas |  BBDD  |   FOL   |");
+        System.out.println("+------------------------------------------------------+");
+        System.out.println("|  5-6 |  Prog | Marcas |  Sistemas |  BBDD  |   FOL   |");
+        System.out.println("+------------------------------------------------------+");
+        System.out.println("|  6-7 |                   RECREO                      |");
+        System.out.println("+------------------------------------------------------+");
+    }
+    public static void ejercicio5 (){
+        System.out.println("        *       ");
+        System.out.println("       * *       ");
+        System.out.println("      * * *      ");
+        System.out.println("     * * * *     ");
+        System.out.println("    * * * * *    ");
+        System.out.println("   * * * * * *   ");
+        System.out.println("  * * * * * * *  ");
+        System.out.println(" * * * * * * * * ");
+        System.out.println("* * * * * * * * *");
+
+    }
+    public  static void  ejercicio6 (){
+        System.out.println("        *       ");
+        System.out.println("       * *       ");
+        System.out.println("      *   *      ");
+        System.out.println("     *     *     ");
+        System.out.println("    *       *    ");
+        System.out.println("   *         *   ");
+        System.out.println("  *           *  ");
+        System.out.println(" *             * ");
+        System.out.println("* * * * * * * * *");
+    }
+    public static void ejercicio7() {
+        System.out.println("* * * * * * * * *");
+        System.out.println(" * * * * * * * * ");
+        System.out.println("  * * * * * * *  ");
+        System.out.println("   * * * * * *   ");
+        System.out.println("    * * * * *    ");
+        System.out.println("     * * * *     ");
+        System.out.println("      * * *      ");
+        System.out.println("       * *       ");
+        System.out.println("        *       ");
     }
 }
